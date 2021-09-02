d0b4545bea364541c557acc95f1f91093d5fc640
succip <succip@gmail.com>
2021-09-01 20:23:34 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import React from "react";
+import Header from "./Components/Header/Header";
 
 const App = () => {
   return (
     <React.Fragment>
+      <Header></Header>
       <h3>Welcome to Paolo's Website!!!!</h3>
       <p>
         Here's where you'll find all the stuff I made for the <i>World Wibe Web</i>.
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Header = () => {
+  return (
+    <React.Fragment>
+      <p>This is the header!</p>
+    </React.Fragment>
+  );
+};
+
+export default Header;
