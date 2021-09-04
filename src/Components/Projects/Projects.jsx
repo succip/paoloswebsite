dd787c4b30a7d881f53c0e26d61270279de647a2
succip <succip@gmail.com>
2021-09-04 15:30:33 -0700

    added projects, styled favicon, changed doc title

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 const Projects = () => {
-  return <React.Fragment>List of Projects are found here</React.Fragment>;
+  return (
+    <React.Fragment>
+      <h3>Collection of fun things I've done</h3>
+      <div>
+        <a href="https://shielded-reaches-91505.herokuapp.com/">GeoJSON Downloader</a>
+      </div>
+      <div>
+        <a href="https://cosmos.surrey.ca/external/surreygame/">Surrey Community Guesser</a>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Projects;
