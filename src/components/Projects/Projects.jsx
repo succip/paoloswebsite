7511aefe7183efc5f599b36877d463a8f0af9939
succip <succip@gmail.com>
2021-09-06 21:10:13 -0700

    added assets, edited projects order

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,13 +5,13 @@ const Projects = () => {
     <React.Fragment>
       <h3>Collection of fun things I've done</h3>
       <div>
+        <a href="https://mappolls.netlify.app/">MapPolls</a>
+      </div>
+      <div>
         <a href="https://geojsoncreator.netlify.app/">GeoJSON Creator</a>
       </div>
       <div>
         <a href="https://cosmos.surrey.ca/external/surreygame/">Surrey Community Guesser</a>
-      </div>
-      <div>
-        <a href="https://mappolls.netlify.app/">MapPolls</a>
       </div>
     </React.Fragment>
   );
