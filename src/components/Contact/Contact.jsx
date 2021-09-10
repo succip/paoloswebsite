1bfc53a1e034b4bd74d1f622f515eb549b7e8593
succip <succip@gmail.com>
2021-09-09 22:17:17 -0700

    contact page edit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,29 @@
 const Contact = () => {
-  return <div>Here's where you can contact me</div>;
+  return (
+    <div>
+      <p className="h3">Here's the places you'll find me</p>
+      <p>
+        <a href="https://github.com/succip" target="_blank" rel="noreferrer">
+          GitHub
+        </a>
+      </p>
+      <p>
+        <a href="https://www.linkedin.com/in/paolosucci/" target="_blank" rel="noreferrer">
+          LinkedIn
+        </a>
+      </p>
+      <p>
+        <a href="https://twitter.com/internet_paolo" target="_blank" rel="noreferrer">
+          Twitter
+        </a>
+      </p>
+      <p>
+        <a href="https://twitch.com/internet_paolo" target="_blank" rel="noreferrer">
+          Twitch
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Contact;
